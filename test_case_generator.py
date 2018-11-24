bec4c7993ab6b47942c59fc3ccda36a470c04b85
Ernest Xu <ernest.xu1024@gmail.com>
2018-11-24 15:11:12 +0000

    Delete test_case_generator.py

--- test_case_generator.py
+++
@@ -1,8 +0,0 @@
-with open("test_cases.txt", "w") as file:
-    discount_factors = [1.0, 0.9, 0.8, 0.7, 0.6]
-    safety_thresholds = [-200.0, -300.0, -400.0]
-    ghostbuster_modes = ["inactive", "defensive", "offensive"]
-    for discount_factor in discount_factors:
-        for safety_threshold in safety_thresholds:
-            for ghostbuster_mode in ghostbuster_modes:
-                file.write(str(discount_factor) + ", " + str(safety_threshold) + ", " + str(ghostbuster_mode) + "\n")
