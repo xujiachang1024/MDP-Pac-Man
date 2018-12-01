517efcc79bbf1e39e11ba8b130c2fe449612cb46
Ernest Xu <ernest.xu1024@gmail.com>
2018-12-01 15:41:10 +0000

    Implement MDPAgent v0.1

--- mdpAgents.py
+++ mdpAgents.py
@@ -46,6 +46,8 @@ class MDPAgent(Agent):
     @return None
     """
     def __init__(self):
+        self.__TOP_RIGHT_WALL_CORNER_smallGrid = (6, 6)
+        self.__TOP_RIGHT_WALL_CORNER_mediumClassic = (19, 10)
         self.__CAPSULE = ("CP", 50.0)
         self.__FOOD = ("FD", 10.0)
         self.__WALL = ("WL", 0.0)
@@ -55,8 +57,8 @@ class MDPAgent(Agent):
         self.__CONVERGENT = ("CV", None)
         self.__SAFETY_DISTANCE = 4
         self.__THREAT_DECAY_RATE = 50.0
-        self.__DISCOUNT_FACTOR = 0.6
-        self.__CONVERGENCE_TOLERANCE = 0.0001
+        self.__DISCOUNT_FACTOR = 0.7
+        self.__CONVERGENCE_TOLERANCE = 0.1
         self.__NORMAL_EARLY_STOPPING_POINT = 100
         self.__SPARSE_EARLY_STOPPING_POINT = 200
         self.__INACTIVE_GHOSTBUSTER_MODE = "inactive"
@@ -70,6 +72,7 @@ class MDPAgent(Agent):
         self.__corners = None
         self.__floors = None
         self.__neighbors = None
+        self.__rewards = None
         self.__utilities = None
         self.__counter = 0
         self.__round = 1
@@ -83,6 +86,17 @@ class MDPAgent(Agent):
     """
     def registerInitialState(self, state):
         print("Round " + str(self.__round) + " running...")
+        corners = api.corners(state)
+        # optimal parameter setting for smallGrid
+        if self.__TOP_RIGHT_WALL_CORNER_smallGrid in corners:
+            self.__SAFETY_DISTANCE = 2
+            self.__DISCOUNT_FACTOR = 0.6
+            self.__GHOSTBUSTER_MODE = self.__INACTIVE_GHOSTBUSTER_MODE
+        # optimal parameter setting for mediumClassic
+        elif self.__TOP_RIGHT_WALL_CORNER_mediumClassic in corners:
+            self.__SAFETY_DISTANCE = 4
+            self.__DISCOUNT_FACTOR = 0.7
+            self.__GHOSTBUSTER_MODE = self.__INACTIVE_GHOSTBUSTER_MODE
 
     """
     Reset internal memories and log record at the end of each round
@@ -92,7 +106,7 @@ class MDPAgent(Agent):
     @param log_mode: (optional) a boolean value to indicate whether the log mode is active; the default value is false
     @return None
     """
-    def final(self, state, log_mode=False):
+    def final(self, state, log_mode=False, filename="./testing_data_1/log_1.csv"):
         self.__states = None
         self.__capsules = None
         self.__foods = None
@@ -100,14 +114,14 @@ class MDPAgent(Agent):
         self.__corners = None
         self.__floors = None
         self.__neighbors = None
+        self.__rewards = None
         self.__utilities = None
         self.__counter = 0
         self.__round += 1
         if log_mode:
-            with open("./testing_data/log.csv", "a") as logfile:
+            with open(filename, "a") as log_file:
                 entry = ""
-                entry += str(self.__HOSTILE[1] + (self.__SAFETY_DISTANCE - 1) * self.__THREAT_DECAY_RATE) + ", "
-                entry += str(self.__THREAT_DECAY_RATE) + ", "
+                entry += str(self.__SAFETY_DISTANCE) + ", "
                 entry += str(self.__DISCOUNT_FACTOR) + ", "
                 entry += str(self.__CONVERGENCE_TOLERANCE) + ", "
                 entry += str(self.__NORMAL_EARLY_STOPPING_POINT) + ", "
@@ -120,7 +134,7 @@ class MDPAgent(Agent):
                 else:
                     entry += ", "
                 entry += str(state.getScore()) + "\n"
-                logfile.write(entry)
+                log_file.write(entry)
 
     """
     Map the maze by building internal memories of the agent
@@ -193,11 +207,12 @@ class MDPAgent(Agent):
 			self.__counter = self.__corners.index(agent_location)
 			self.__counter += random.choice([1, 2, 3])
 
-    """False
-    Initialize data structures for value iteration of expected utilities
+    """
+    Initialize data structures of rewards and utilities for value iteration on Bellman's Equation
 
     @param self: the class itself
-    @param foods: a set of locations of foods
+    @param targets: a set of locations of targets
+    @param target_value: a tuple of (1) target identifier, and (2) reward value
     @param walls: a list of locations of walls
     @param floors: a list of locations of floors (free spaces)
     @param ghosts: a list of locations of ghosts
@@ -205,28 +220,34 @@ class MDPAgent(Agent):
     @param threat_decay_rate: (optional) the decay rate of negative threat utility initialized to spaces surrounding a ghost; the default value is 50.0
     @return
     """
-    def __initialize_utilities(self, targets, target_utility, walls, floors, ghosts, safety_distance=4, threat_decay_rate=50.0):
+    def __initialize_data_structures(self, targets, target_value, walls, floors, ghosts, safety_distance=4, threat_decay_rate=50.0):
+        # initialize empty data structures
+        rewards = dict()
         utilities = dict()
-        # assign 0.0 utility value to walls
+        # assign 0.0 reward value to each wall
+        # assign 0.0 utility value to each wall
         for wall in walls:
+            rewards[wall] = self.__WALL
             utilities[wall] = self.__WALL
-        # assign 0.0 utility value to floors (free spaces)
-        # assign +10.0 utility value to targets
+        # assign 0.0 reward value to each free space
+        # assign +10.0 reward value to each target
+        # assign 0.0 utility value each floor
         for floor in floors:
-            utilities[floor] = self.__FREE
+            rewards[floor] = self.__FREE
             if floor in targets:
-                utilities[floor] = target_utility
-        # assign -500.0 utility value to ghosts
-        # assign decaying threat utility values recursively to locations surrounding each ghost
+                rewards[floor] = target_value
+            utilities[floor] = self.__FREE
+        # assign -500.0 reward value to each ghost
+        # assign decaying threat reward values recursively to locations surrounding each ghost
         for ghost in ghosts:
             ghost = (int(ghost[0]), int(ghost[1]))
-            utilities[ghost] = self.__HOSTILE
+            rewards[ghost] = self.__HOSTILE
             queue = util.Queue()
             queue.push(ghost)
             visited = set([ghost])
             while not queue.isEmpty():
                 curr_location = queue.pop()
-                if utilities[curr_location][1] >= (self.__HOSTILE[1] + threat_decay_rate * (safety_distance - 1)) or utilities[curr_location][1] >= (0.0 - threat_decay_rate):
+                if rewards[curr_location][1] >= (self.__HOSTILE[1] + threat_decay_rate * (safety_distance - 1)) or rewards[curr_location][1] >= (0.0 - threat_decay_rate):
                     continue
                 displacements = [(1, 0), (-1, 0), (0, 1), (0, -1)]
                 for displacement in displacements:
@@ -235,12 +256,12 @@ class MDPAgent(Agent):
                         continue
                     if next_location in visited:
                         continue
-                    if utilities[next_location][1] <= utilities[curr_location][1]:
+                    if rewards[next_location][1] <= rewards[curr_location][1]:
                         continue
-                    utilities[next_location] = (self.__HOSTILE[0], utilities[curr_location][1] + threat_decay_rate)
+                    rewards[next_location] = (self.__HOSTILE[0], rewards[curr_location][1] + threat_decay_rate)
                     queue.push(next_location)
                     visited.add(next_location)
-        return utilities
+        return (rewards, utilities)
 
     """
     Update expected utilities based on Bellman's Equation (Note: just 1 iteration)
@@ -253,9 +274,10 @@ class MDPAgent(Agent):
     @param convergence_tolerance: (optional) the threshold to decide whether the expected utility of each location converges; the default value is 0.0001
     @return
     """
-    def __update_utilities(self, walls, neighbors, utilities, discount_factor=1.0, convergence_tolerance=0.0001, ignoring_walls=False, maximum_mode=True):
+    def __update_utilities(self, walls, neighbors, rewards, utilities, discount_factor=1.0, convergence_tolerance=0.0001, ignoring_walls=False, maximum_mode=True):
         previous_utilities = copy.deepcopy(utilities) # old version of utilities
         fully_convergent = True
+        total_entropy = 0.0
         for location in utilities.keys():
             if utilities[location][0] == self.__FREE[0]:
                 fully_convergent = False
@@ -331,24 +353,27 @@ class MDPAgent(Agent):
                     # right of south: west
                     if neighbors[location][Directions.WEST] not in walls:
                         south_utility += 0.1 * previous_utilities[neighbors[location][Directions.WEST]][1]
-                """update expected food utility"""
+                """update expected utility"""
                 if maximum_mode:
-                    utilities[location] = (previous_utilities[location][0], -1 + discount_factor * max([east_utility, west_utility, north_utility, south_utility]))
+                    utilities[location] = (previous_utilities[location][0], rewards[location][1] + discount_factor * max([east_utility, west_utility, north_utility, south_utility]))
                 else:
-                    utilities[location] = (previous_utilities[location][0], -1 + discount_factor * min([east_utility, west_utility, north_utility, south_utility]))
-                if abs(utilities[location][1] - previous_utilities[location][1]) < convergence_tolerance:
-                    utilities[location] = (self.__CONVERGENT[0], utilities[location][1])
-        return (utilities, fully_convergent)
+                    utilities[location] = (previous_utilities[location][0], rewards[location][1] + discount_factor * min([east_utility, west_utility, north_utility, south_utility]))
+                # if abs(utilities[location][1] - previous_utilities[location][1]) < convergence_tolerance:
+                #     utilities[location] = (self.__CONVERGENT[0], utilities[location][1])
+                total_entropy += abs(utilities[location][1] - previous_utilities[location][1])
+            if total_entropy < convergence_tolerance:
+                fully_convergent = True
+        return (utilities, fully_convergent, total_entropy)
 
     """
-    Print the utilities dictionary in a user-friendly display
+    Print the data structure of utilities or rewards in a user-friendly grid display
 
-    @param self: the class ifself
+    @param self: the class itself
     @param walls: a list of locations of walls
-    @param utilities: a dictionary that point from a location (key) to its utilities (value)
+    @param grid: a dictionary that point from a location (key) to its reward or utility (value)
     @return None
     """
-    def __print_utilities(self, walls, utilities):
+    def __print_data_structure(self, walls, grid):
         x_coordinates = []
         y_coordinates = []
         for wall in walls:
@@ -359,12 +384,12 @@ class MDPAgent(Agent):
         for y in range(y_maximum, y_minimum - 1, -1):
             line = "{:4s}:".format("y=" + str(y))
             for x in range(x_minimum, x_maximum + 1, 1):
-                utility = utilities[(x, y)]
-                line += "({:2s},{:>8s})".format(utility[0], "{:+4.3f}".format(utility[1]))
+                grid_value = grid[(x, y)]
+                line += "({:2s},{:>13s})".format(grid_value[0], "{:+9.3f}".format(grid_value[1]))
             print(line)
         x_axis = "     "
         for x in range(x_minimum, x_maximum + 1, 1):
-            x_axis += "{:13s}".format(" x=" + str(x))
+            x_axis += "{:18s}".format(" x=" + str(x))
         print(x_axis)
 
     """
@@ -376,8 +401,8 @@ class MDPAgent(Agent):
     @param ghostbuster_mode: (optional) a string to indicate which ghostbuster mode (inactive/defensive/offensive) is in use; the default value is inactive
     @return None
     """
-    def __value_iteration(self, state, debug_mode, ghostbuster_mode="inactive"):
-        """prepare for value iteration"""
+    def __value_iteration(self, state, debug_mode, deep_debug_mode, ghostbuster_mode):
+        """initialize data structures for value iteration"""
         ghosts_states = api.ghostStates(state)
         edible_ghosts = []
         hostile_ghosts = []
@@ -391,32 +416,39 @@ class MDPAgent(Agent):
             print("\tedible_ghosts=" + str(edible_ghosts))
             print("\thostile_ghosts=" + str(hostile_ghosts))
         # inactive ghostbuster mode
-        self.__utilities = self.__initialize_utilities(self.__foods, self.__FOOD, self.__walls, self.__floors, hostile_ghosts, safety_distance=self.__SAFETY_DISTANCE, threat_decay_rate=self.__THREAT_DECAY_RATE)
+        self.__rewards, self.__utilities = self.__initialize_data_structures(self.__foods, self.__FOOD, self.__walls, self.__floors, hostile_ghosts, safety_distance=self.__SAFETY_DISTANCE, threat_decay_rate=self.__THREAT_DECAY_RATE)
         early_stopping_point = self.__NORMAL_EARLY_STOPPING_POINT
         if len(self.__foods) < 10:
             early_stopping_point = self.__SPARSE_EARLY_STOPPING_POINT
         # defensive ghostbuster mode
         if ghostbuster_mode == self.__DEFENSIVE_GHOSTBUSTER_MODE:
             if len(edible_ghosts) > 0:
-                self.__utilities = self.__initialize_utilities(edible_ghosts, self.__EDIBLE, self.__walls, self.__floors, hostile_ghosts, safety_threshold=self.__SAFETY_THRESHOLD, threat_decay_rate=self.__THREAT_DECAY_RATE)
+                self.__rewards, self.__utilities = self.__initialize_data_structures(edible_ghosts, self.__EDIBLE, self.__walls, self.__floors, hostile_ghosts, safety_distance=self.__SAFETY_DISTANCE, threat_decay_rate=self.__THREAT_DECAY_RATE)
                 early_stopping_point = self.__SPARSE_EARLY_STOPPING_POINT
         # offensive ghostbuster_mode
         if ghostbuster_mode == self.__OFFENSIVE_GHOSTBUSTER_MODE:
             if len(edible_ghosts) > 0:
-                self.__utilities = self.__initialize_utilities(edible_ghosts, self.__EDIBLE, self.__walls, self.__floors, hostile_ghosts, safety_threshold=self.__SAFETY_THRESHOLD, threat_decay_rate=self.__THREAT_DECAY_RATE)
+                self.__rewards, self.__utilities = self.__initialize_data_structures(edible_ghosts, self.__EDIBLE, self.__walls, self.__floors, hostile_ghosts, safety_distance=self.__SAFETY_DISTANCE, threat_decay_rate=self.__THREAT_DECAY_RATE)
                 early_stopping_point = self.__SPARSE_EARLY_STOPPING_POINT
             elif len(self.__capsules) > 0:
-                self.__utilities = self.__initialize_utilities(self.__capsules, self.__CAPSULE, self.__walls, self.__floors, hostile_ghosts, safety_threshold=self.__SAFETY_THRESHOLD, threat_decay_rate=self.__THREAT_DECAY_RATE)
+                self.__rewards, self.__utilities = self.__initialize_data_structures(self.__capsules, self.__CAPSULE, self.__walls, self.__floors, hostile_ghosts, safety_distance=self.__SAFETY_DISTANCE, threat_decay_rate=self.__THREAT_DECAY_RATE)
                 early_stopping_point = self.__SPARSE_EARLY_STOPPING_POINT
         """use value iteration to update utilites until convergence or early stopping point"""
+        stopping_point = None
         for i in range(early_stopping_point):
-            self.__utilities, fully_convergent = self.__update_utilities(self.__walls, self.__neighbors, self.__utilities, discount_factor=self.__DISCOUNT_FACTOR, convergence_tolerance=self.__CONVERGENCE_TOLERANCE, ignoring_walls=False, maximum_mode=True)
+            stopping_point = i + 1
+            self.__utilities, fully_convergent, total_entropy = self.__update_utilities(self.__walls, self.__neighbors, self.__rewards, self.__utilities, discount_factor=self.__DISCOUNT_FACTOR, convergence_tolerance=self.__CONVERGENCE_TOLERANCE, ignoring_walls=False, maximum_mode=True)
+            if deep_debug_mode:
+                self.__print_data_structure(self.__walls, self.__rewards)
+                self.__print_data_structure(self.__walls, self.__utilities)
+                print("\ttotal_entropy=" + "{:+10.3f}".format(total_entropy))
             if fully_convergent:
-                if debug_mode:
-                    print("\t#iterations=" + str(i))
                 break
         if debug_mode:
-            self.__print_utilities(self.__walls, self.__utilities)
+            self.__print_data_structure(self.__walls, self.__rewards)
+            self.__print_data_structure(self.__walls, self.__utilities)
+            print("\ttotal_entropy=" + "{:+10.3f}".format(total_entropy))
+            print("\tstopping_point=" + str(stopping_point))
 
     """
     Use maximum expected utility to decide which direction to go next
@@ -460,11 +492,14 @@ class MDPAgent(Agent):
     @param debug_mode: (optional) a boolean value to indicate whether the debug mode is active; the default value is false
     @return the direction the agent decides to go (non-deterministic)
     """
-    def getAction(self, state, debug_mode=False):
+    def getAction(self, state, debug_mode=True, deep_debug_mode=False):
         """mapping operation"""
         self.__mapping_operation(state, debug_mode=debug_mode)
         """value iteration"""
-        self.__value_iteration(state, debug_mode=debug_mode, ghostbuster_mode=self.__GHOSTBUSTER_MODE)
+        self.__value_iteration(state, debug_mode=debug_mode, deep_debug_mode=deep_debug_mode, ghostbuster_mode=self.__GHOSTBUSTER_MODE)
         """maximum expected utility"""
         meu_action = self.__maximum_expected_utility(state, debug_mode=debug_mode)
+        """step-by-step demo"""
+        if deep_debug_mode:
+            breaker = raw_input()
         return api.makeMove(meu_action, api.legalActions(state))
